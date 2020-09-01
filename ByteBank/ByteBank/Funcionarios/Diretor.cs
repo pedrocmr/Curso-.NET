252df360c9007172912f7157eba835a1feba13da
Pedro Camara <57800238+pedrocmr@users.noreply.github.com>
2020-09-01 08:21:24 -0300

    modulo 3*

---
+++ ByteBank/ByteBank/Funcionarios/Diretor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    public class Diretor : Funcionario
+    {
+        public Diretor(string cpf) : base(cpf)
+        {
+            
+        }
+        public override double GetBonificacao()
+        {
+            return Salario + base.GetBonificacao();
+        }
+    }
+}
---
+++ ByteBank/ByteBank/Program.cs
@@ -0,0 +1,46 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+
+            Funcionario pedro = new Funcionario("123.546.856-43");
+            
+
+            pedro.Nome = "Pedro";
+            
+            pedro.Salario = 500;
+
+            gerenciador.Registrar(pedro);
+
+            Console.WriteLine(pedro.GetBonificacao());
+            Console.WriteLine(gerenciador.GetTotalBonificacao());
+
+            Diretor saulo = new Diretor("313.321.325-54");
+            saulo.Nome = "Saulo";
+            saulo.Salario = 4000;
+
+            Funcionario helio = saulo;
+
+            Console.WriteLine("A bonificacao de helio é: " + helio.GetBonificacao());
+            Console.WriteLine("A bonificacao de saulo é: " + saulo.GetBonificacao());
+
+            gerenciador.Registrar(saulo);
+
+            Console.WriteLine(Funcionario.TotalDeFuncionarios);
+            Console.WriteLine(saulo.GetBonificacao());
+            Console.WriteLine(gerenciador.GetTotalBonificacao());
+
+            Console.ReadLine();
+        }
+    }
+}
