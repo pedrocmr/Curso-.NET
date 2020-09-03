e9707617904ed5290461fff0fec1a905c62356e7
Pedro Camara <57800238+pedrocmr@users.noreply.github.com>
2020-09-03 17:10:06 -0300

    modulo 4

--- ByteBankExceptions/ByteBank/ContaCorrente.cs
+++ ByteBankExceptions/ByteBank/ContaCorrente.cs
@@ -1,31 +1,20 @@
 ﻿
+using System;
+
 namespace ByteBank
 {
     public class ContaCorrente
     {
+        public static double TaxaOperacao { get; private set; }
+        
         public Cliente Titular { get; set; }
 
         public static int TotalDeContasCriadas { get; private set; }
 
 
-        private int _agencia;
-        public int Agencia
-        {
-            get
-            {
-                return _agencia;
-            }
-            set
-            {
-                if (value <= 0)
-                {
-                    return;
-                }
-
-                _agencia = value;
-            }
-        }
-        public int Numero { get; set; }
+        
+        public int Agencia { get; }
+        public int Numero { get; }
 
         private double _saldo = 100;
 
@@ -49,22 +38,32 @@ namespace ByteBank
 
         public ContaCorrente(int agencia, int numero)
         {
+            if (agencia <= 0)
+            {
+                throw new ArgumentException("Agencia possui numero 0, que é inválido. ", nameof(agencia));
+            }
+            if (numero <= 0 )
+            {
+                throw new ArgumentException("Numero possui numero 0, que é invalido. ", nameof(numero));
+            }
             Agencia = agencia;
             Numero = numero;
-
+     
             TotalDeContasCriadas++;
+            TaxaOperacao = 30 / TotalDeContasCriadas;
         }
 
 
-        public bool Sacar(double valor)
+        public void Sacar(double valor)
         {
+            
             if (_saldo < valor)
             {
-                return false;
+                throw new ArgumentException("Valor inválido para o saque.", nameof(valor));
             }
 
             _saldo -= valor;
-            return true;
+            
         }
 
         public void Depositar(double valor)
@@ -73,16 +72,16 @@ namespace ByteBank
         }
 
 
-        public bool Transferir(double valor, ContaCorrente contaDestino)
+        public void Transferir(double valor, ContaCorrente contaDestino)
         {
             if (_saldo < valor)
             {
-                return false;
+                throw new ArgumentException("Valor inválido para a transferencia.", nameof(valor));
             }
 
-            _saldo -= valor;
+            Sacar(valor);
             contaDestino.Depositar(valor);
-            return true;
+            
         }
     }
 }
--- ByteBankExceptions/ByteBank/Program.cs
+++ ByteBankExceptions/ByteBank/Program.cs
@@ -6,11 +6,64 @@ using System.Threading.Tasks;
 
 namespace ByteBank
 {
-    class Program
+     class Program
     {
-        static void Main(string[] args)
+         static void Main(string[] args)
         {
+            try
+            {
+                ContaCorrente conta = new ContaCorrente(219,321321);
+                conta.Depositar(100);
+                conta.Sacar(1000);
+                 
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Argumento que tá com problema: " + ex.ParamName);
+                Console.WriteLine("Ocorreu um erro de ArgumentException");
+                Console.WriteLine(ex.Message);
+            }
+            catch (SaldoInsuficienteException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Saldo Insuficiente para completar a operação. ");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+             
             Console.ReadLine();
+        }
+        //Teste com a cadeia de chamada:
+        //Metodo -> TestaDivisao -> Dividir
+        private static void Metodo()
+        {
+            TestaDivisao(0);
+        }
+
+        private static void TestaDivisao(int divisor)
+        {
+
+            int resultado = Dividir(10, divisor);
+            Console.WriteLine("O resultado da divisao por 10 e: " + divisor + " é: " + resultado);
+        }
+            
+      
+
+        private static int Dividir(int numero, int divisor)
+        {
+            try
+            {
+                return numero / divisor;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Exceção qnd divide " + numero + " por " + divisor);
+                throw;
+            }
+         
+            
         }
     }
 }
