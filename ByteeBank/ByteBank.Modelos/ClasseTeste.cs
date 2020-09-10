7907bcd165f676f64d35994f251c37d53e5c0036
Pedro Camara <57800238+pedrocmr@users.noreply.github.com>
2020-09-10 16:48:18 -0300

    modulo 6~9

---
+++ ByteeBank/ByteBank.Modelos/ClasseTeste.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Modelos
+{
+    class ClasseTeste
+    {
+        public ClasseTeste()
+        {
+            ModificadoresTeste teste = new ModificadoresTeste();
+            teste.MetodoPublico();
+            
+
+        }
+    }
+
+    public class ClasseDerivada : ModificadoresTeste
+    {
+        public ClasseDerivada()
+        {
+            MetodoProtegido();
+        }
+    }
+
+
+    public class ModificadoresTeste
+    {
+        public void MetodoPublico()
+        {
+            //Visível fora da classe "ModificadoresTeste"
+        }
+        private void MetodoPrivado()
+        {
+            //Visível apenas na classe "ModificadoresTeste"
+        }
+        protected void MetodoProtegido()
+        {
+
+        }
+        internal void MetodoInterno()
+        {
+            //Visível apenas no projeto ByteBank.Modelos
+        }
+    }
+
+}
